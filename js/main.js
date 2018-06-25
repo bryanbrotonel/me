7702fefdc9d8a96a7bdef143e9950cba9fb4947b
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-06-24 21:07:46 -0700

    Added work page layout

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+// Scroll Reveal
+window.sr = ScrollReveal();
+sr.reveal('.work-item', {
+  duration: 500,
+  delay: 500,
+  distance: '3px',
+  mobile: true,
+  scale: 0.98
+}, 150);
+
+sr.reveal('.about-icon', {
+  duration: 500,
+  delay: 500,
+  distance: '3px',
+  mobile: true,
+  scale: 0.98
+}, 150);
+
+// Typed JS
+var typed = new Typed(".typed", {
+  strings: [
+    "<b>I build websites", "I build <b>beautiful</b> websites", "I build <b>clean</b> websites", "I build <b>responsive</b> websites", "I build websites for <b>you</b> ^1250 <span class='ec ec-yellow-heart'></span>"
+
+  ],
+  typeSpeed: 30,
+  backSpeed: 30,
+  startDelay: 1000,
+  backDelay: 1500,
+  showCursor: false,
+  smartBackspace: true
+});
+
+// Animate CSS
 $.fn.extend({
   animateCss: function(animationName, callback) {
     var animationEnd = (function(el) {
@@ -27,27 +60,25 @@ $.fn.extend({
   }
 });
 
-window.sr = ScrollReveal();
-sr.reveal('.work-item', {
-  duration: 500,
-  distance: '3px',
-  mobile: true,
-  scale: 0.98
-}, 150);
-
-var typed = new Typed(".typed", {
-  strings: [
-    "I make websites", "I make beautiful websites", "I make clean websites", "I make responsive websites", "I make websites for you"
-  ],
-  typeSpeed: 40,
-  backSpeed: 35,
-  startDelay: 1200,
-  backDelay: 2500,
-  showCursor: false,
-  contentType: 'h1',
-  smartBackspace: true // Default value
-});
-
 $('.fa-2x').hover(function() {
   $(this).animateCss('pulse');
+});
+
+$('.banner-arrow').hover(function() {
+  $(this).animateCss('bounce');
+});
+
+// Sroll to SECTION
+// Smooth scrolling using jQuery easing
+$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: (target.offset().top)
+      }, 1000, "easeInOutExpo");
+      return false;
+    }
+  }
 });
