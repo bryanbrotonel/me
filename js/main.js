d908bae17924f28b19d5b1d8a6e0161a729970b9
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-07-16 21:25:22 -0700

    Updated home page layout

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@
 window.sr = ScrollReveal();
 sr.reveal('.work-item', {
   duration: 500,
-  delay: 500,
-  distance: '3px',
+  delay: 250,
+  distance: '2px',
   mobile: true,
   scale: 0.98
-}, 150);
+});
 
-sr.reveal('.about-icon', {
+sr.reveal('.footer-icon', {
   duration: 500,
   delay: 500,
   distance: '3px',
@@ -16,53 +16,39 @@ sr.reveal('.about-icon', {
   scale: 0.98
 }, 150);
 
-// Typed JS
-var typed = new Typed(".typed", {
-  strings: [
-    "<b>I build websites", "I build <b>beautiful</b> websites", "I build <b>clean</b> websites", "I build <b>responsive</b> websites", "I build websites for <b>you</b> ^1250 <span class='ec ec-yellow-heart'></span>"
-
-  ],
-  typeSpeed: 30,
-  backSpeed: 30,
-  startDelay: 1000,
-  backDelay: 1500,
-  showCursor: false,
-  smartBackspace: true
-});
-
-// Animate CSS
-$.fn.extend({
-  animateCss: function(animationName, callback) {
-    var animationEnd = (function(el) {
-      var animations = {
-        animation: 'animationend',
-        OAnimation: 'oAnimationEnd',
-        MozAnimation: 'mozAnimationEnd',
-        WebkitAnimation: 'webkitAnimationEnd'
-      };
-
-      for (var t in animations) {
-        if (el.style[t] !== undefined) {
-          return animations[t];
-        }
-      }
-    })(document.createElement('div'));
-
-    this.addClass('animated ' + animationName).one(animationEnd, function() {
-      $(this).removeClass('animated ' + animationName);
-
-      if (typeof callback === 'function')
-        callback();
-      }
-    );
-
-    return this;
-  }
-});
-
-$('.fa-2x').hover(function() {
-  $(this).animateCss('pulse');
-});
+// // Animate CSS
+// $.fn.extend({
+//   animateCss: function(animationName, callback) {
+//     var animationEnd = (function(el) {
+//       var animations = {
+//         animation: 'animationend',
+//         OAnimation: 'oAnimationEnd',
+//         MozAnimation: 'mozAnimationEnd',
+//         WebkitAnimation: 'webkitAnimationEnd'
+//       };
+//
+//       for (var t in animations) {
+//         if (el.style[t] !== undefined) {
+//           return animations[t];
+//         }
+//       }
+//     })(document.createElement('div'));
+//
+//     this.addClass('animated ' + animationName).one(animationEnd, function() {
+//       $(this).removeClass('animated ' + animationName);
+//
+//       if (typeof callback === 'function')
+//         callback();
+//       }
+//     );
+//
+//     return this;
+//   }
+// });
+//
+// $('.fa-2x').hover(function() {
+//   $(this).animateCss('pulse');
+// });
 
 $('.banner-arrow').hover(function() {
   $(this).animateCss('bounce');
