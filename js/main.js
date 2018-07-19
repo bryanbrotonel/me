744cb651720c8dfd68f7140821ece6dcf6205007
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-07-18 21:59:26 -0700

    Added home page content

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 // Scroll Reveal
 window.sr = ScrollReveal();
-sr.reveal('.work-item', {
-  duration: 500,
-  delay: 250,
-  distance: '2px',
+sr.reveal('.work-section', {
+  duration: 800,
+  delay: 400,
+  distance: '1px',
   mobile: true,
-  scale: 0.98
+  scale: 0.999
 });
+
 
 sr.reveal('.footer-icon', {
   duration: 500,
@@ -16,39 +17,39 @@ sr.reveal('.footer-icon', {
   scale: 0.98
 }, 150);
 
-// // Animate CSS
-// $.fn.extend({
-//   animateCss: function(animationName, callback) {
-//     var animationEnd = (function(el) {
-//       var animations = {
-//         animation: 'animationend',
-//         OAnimation: 'oAnimationEnd',
-//         MozAnimation: 'mozAnimationEnd',
-//         WebkitAnimation: 'webkitAnimationEnd'
-//       };
-//
-//       for (var t in animations) {
-//         if (el.style[t] !== undefined) {
-//           return animations[t];
-//         }
-//       }
-//     })(document.createElement('div'));
-//
-//     this.addClass('animated ' + animationName).one(animationEnd, function() {
-//       $(this).removeClass('animated ' + animationName);
-//
-//       if (typeof callback === 'function')
-//         callback();
-//       }
-//     );
-//
-//     return this;
-//   }
-// });
-//
-// $('.fa-2x').hover(function() {
-//   $(this).animateCss('pulse');
-// });
+// Animate CSS
+setTimeout(function () {
+    $('.banner-content').show().addClass('animated fadeIn');}, 650
+);
+
+$.fn.extend({
+  animateCss: function(animationName, callback) {
+    var animationEnd = (function(el) {
+      var animations = {
+        animation: 'animationend',
+        OAnimation: 'oAnimationEnd',
+        MozAnimation: 'mozAnimationEnd',
+        WebkitAnimation: 'webkitAnimationEnd'
+      };
+
+      for (var t in animations) {
+        if (el.style[t] !== undefined) {
+          return animations[t];
+        }
+      }
+    })(document.createElement('div'));
+
+    this.addClass('animated ' + animationName).one(animationEnd, function() {
+      $(this).removeClass('animated ' + animationName);
+
+      if (typeof callback === 'function')
+        callback();
+      }
+    );
+
+    return this;
+  }
+});
 
 $('.banner-arrow').hover(function() {
   $(this).animateCss('bounce');
