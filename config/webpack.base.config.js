9fc40f44d009b62e9b07ef4ba5c37bee22eb2cf0
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2019-02-06 21:25:03 -0800

    Configured program for Netlify deployment

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -4,6 +4,7 @@ const merge = require("webpack-merge");
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const APP_DIR = path.resolve(__dirname, "../src");
 
@@ -59,7 +60,8 @@ module.exports = env => {
         new webpack.DefinePlugin({
           "process.env.VERSION": JSON.stringify(env.VERSION),
           "process.env.PLATFORM": JSON.stringify(env.PLATFORM)
-        })
+        }),
+        new CopyWebpackPlugin([{ from: "src/static" }])
       ]
     }
   ]);
