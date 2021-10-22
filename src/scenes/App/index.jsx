3f482193e82b21b96e81c66cad1fc28d4b97678d
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2021-10-22 10:30:25 -0700

    Added contenful with react hooks

--- src/scenes/App/index.jsx
+++ src/scenes/App/index.jsx
@@ -1,24 +1,61 @@
-import React from "react";
-import Title from "components/Title";
-import ProfileBlurb from "components/ProfileBlurb";
-import ProfilePicture from "components/ProfilePicture";
-import Socials from "components/Socials";
+import React, { useState, useEffect } from 'react';
 
-import "./styles.scss";
+import './styles.scss';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <Title />
-          <ProfilePicture />
-          <ProfileBlurb />
-          <Socials />
-        </div>
-      </div>
-    );
+const query = `
+  {
+    mastheadCollection{
+      items {
+        title
+        image{
+          url
+        }
+        blurb
+      }
+    }
   }
+  `;
+
+function App() {
+  const [page, setPage] = useState(null);
+
+  useEffect(() => {
+    window
+      .fetch('https://graphql.contentful.com/content/v1/spaces/yg6bvazmunnt/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // Auth request
+          Authorization:
+            'Bearer a0257b82968ffe4ec3d59ee2087eea1f82eabd5b0a2c50e9737aeadceb95d24d',
+        },
+        // Send GraphQL query
+        body: JSON.stringify({ query }),
+      })
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+
+        // render entire component with new data
+        setPage(data.mastheadCollection.items[0]);
+      });
+  }, []);
+
+  if (!page) return 'Loading...';
+
+  const { title, image, blurb } = page;
+
+  return (
+    <div>
+      <div>
+        <h1>{title}</h1>
+        <img src={image.url} alt="Image" />
+        <p>{blurb}</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
