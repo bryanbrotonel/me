a6f173cd84266f9f3aa107ac14e79fef0969e1d7
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2022-08-21 13:17:38 -0700

    Updated webpack config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,33 +1,10 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  optimization: {
-    minimizer: [
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.imageminMinify,
-          options: {
-            plugins: [['optipng', { optimizationLevel: 5 }]],
-          },
-        },
-        generator: [
-          {
-            // You can apply generator using `?as=webp`, you can use any name and provide more options
-            preset: 'webp',
-            implementation: ImageMinimizerPlugin.imageminGenerate,
-            options: {
-              plugins: ['imagemin-webp'],
-            },
-          },
-        ],
-      }),
-    ],
-  },
   plugins: [
     new MiniCssExtractPlugin(),
     new CopyPlugin({
