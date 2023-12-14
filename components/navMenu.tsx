1edcd6869d95c21aa67fc1ba9775467380593b23
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2023-12-13 21:21:35 -0800

    Added navbar and navmenu styling

--- components/navMenu.tsx
+++ components/navMenu.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 import { Cross } from 'akar-icons';
 
-export default function NevMenu({ toggle }: { toggle: () => void }) {
+export default function NevMenu({
+  toggle,
+  className,
+}: {
+  toggle: () => void;
+  className?: string;
+}) {
   const LinkItem = ({ href, children }) => {
     return (
       <Link href={href} onClick={toggle} className="text-xl text-white">
@@ -10,8 +16,11 @@ export default function NevMenu({ toggle }: { toggle: () => void }) {
       </Link>
     );
   };
+
   return (
-    <div className="fixed z-50 inset-0 space-y-2 bg-black">
+    <div
+      className={`w-screen h-screen fixed inset-0 space-y-2 bg-black/95 backdrop-blur-md overscroll-none ${className}`}
+    >
       <div className="text-right p-4">
         <button onClick={toggle}>
           <Cross className="text-white" strokeWidth={2.5} size={20} />
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ThreeLineHorizontal } from 'akar-icons';
 import NavMenu from './navMenu';
 import ActiveLink from './activeLink';
@@ -10,32 +10,46 @@ export default function Navbar() {
     setIsNavMenuOpen(!isNavMenuOpen);
   };
 
+  // Disable scrolling when nav menu is open
+  useEffect(() => {
+    document.body.style.overflow = isNavMenuOpen ? 'hidden' : 'unset';
+  }, [isNavMenuOpen]);
+
   return (
-    <div className="w-auto flex md:flex-col justify-between p-4 md:p-0 md:space-y-4 items-start bg-white border-b-2 border-slate-100 md:border-0">
-      <div className="space-y-1 md:p-0">
-        <div className="w-fit md:mb-5">
-          <Link href="/work">
-            <h1 className="text-xl md:text-5xl font-bold">BryanBrotonel</h1>
-          </Link>
+    <>
+      <div className="w-auto flex md:flex-col justify-between p-4 md:p-0 md:space-y-4 items-start backdrop-blur-md bg-white/80">
+        <div className="space-y-1 md:p-0">
+          <div className="w-fit md:mb-5">
+            <Link href="/work">
+              <h1 className="text-xl md:text-5xl font-bold">BryanBrotonel</h1>
+            </Link>
+          </div>
+          <h2 className="text-sm md:text-lg">
+            Web Developer from Vancouver, BC
+          </h2>
         </div>
-        <h2 className="text-sm md:text-lg">Web Developer from Vancouver, BC</h2>
+        <div className="block md:hidden" onClick={toggleNavMenu}>
+          <ThreeLineHorizontal strokeWidth={2.5} size={20} />
+        </div>
+        <div className="hidden md:flex gap-4">
+          <ActiveLink
+            activeClassName="font-bold"
+            activePathnames={['/work', '/']}
+            href="/work"
+          >
+            Work
+          </ActiveLink>
+          <ActiveLink activeClassName="font-bold" href="/about">
+            About
+          </ActiveLink>
+        </div>
       </div>
-      <div className="block md:hidden" onClick={toggleNavMenu}>
-        <ThreeLineHorizontal strokeWidth={2.5} size={20} />
-      </div>
-      <div className="hidden md:flex gap-4">
-        <ActiveLink
-          activeClassName="font-bold"
-          activePathnames={['/work', '/']}
-          href="/work"
-        >
-          Work
-        </ActiveLink>
-        <ActiveLink activeClassName="font-bold" href="/about">
-          About
-        </ActiveLink>
-      </div>
-      {isNavMenuOpen && <NavMenu toggle={toggleNavMenu} />}
-    </div>
+      <NavMenu
+        toggle={toggleNavMenu}
+        className={`${
+          isNavMenuOpen ? 'translate-x-0' : 'translate-x-full'
+        } transition-transform duration-500 ease-in-out`}
+      />
+    </>
   );
 }
