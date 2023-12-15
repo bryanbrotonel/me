772756f9fc1f93e87bfade10ca8c57fbe293633c
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2023-12-14 22:18:46 -0800

    Fixed api write end error

--- pages/api/draft.ts
+++ pages/api/draft.ts
@@ -5,13 +5,13 @@ export default async function handler(
   response: NextApiResponse,
 ) {
   if (request.method !== 'GET') {
-    response.status(401).json({ message: 'Invalid method' });
+    return response.status(401).json({ message: 'Invalid method' });
   }
 
   const { secret } = request.query;
 
   if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET) {
-    response.status(401).json({ message: 'Invalid token' });
+    return response.status(401).json({ message: 'Invalid token' });
   }
 
   response.setDraftMode({ enable: true });
