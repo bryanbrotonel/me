dc794a63a436e2b827be8840eaa3cbfb12875a5c
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2024-02-29 21:54:57 -0800

    Fixed spotify api fetching logic

--- app/lib/api/spotify.ts
+++ app/lib/api/spotify.ts
@@ -43,60 +43,47 @@ const getRecentlyPlayed = async (access_token: string) => {
   });
 };
 
+async function createSongObject(song: any) {
+  const title = song.name;
+  const album = song.album.name;
+  const artists = song.artists.map((artist: any) => artist.name).join(', ');
+  const spotifyUrl = song.external_urls.spotify ?? null;
+  const albumImageUrl = song.album.images[0]?.url ?? null;
+
+  return {
+    title,
+    album,
+    albumImageUrl,
+    artists,
+    spotifyUrl,
+  };
+}
+
 export async function getListeningStatus(): Promise<spotifyListeningProps> {
   const { access_token } = await getAccessToken();
   const nowPlaying = await getNowPlaying(access_token);
 
-  if (nowPlaying.status > 204) {
-    return { isPlaying: false };
+  if (nowPlaying.status < 204) {
+    const song = await nowPlaying.json();
+    return {
+      song: await createSongObject(song.item),
+      isPlaying: true,
+    };
   }
 
-  if (nowPlaying.status === 204) {
-    const recentlyPlayed = await getRecentlyPlayed(access_token);
+  const recentlyPlayed = await getRecentlyPlayed(access_token);
+
+  if (recentlyPlayed.status < 204) {
     const data = await recentlyPlayed.json();
     const recentSong = data.items[0];
-
     const timestamp = recentSong.played_at?.toString();
-    const title = recentSong.track.name;
-    const album = recentSong.track.album.name;
-    const artists = recentSong.track.artists
-      .map((_artist: any) => _artist.name)
-      .join(', ');
-
-    let spotifyUrl = recentSong.track.external_urls.spotify ?? null;
-    let albumImageUrl = recentSong.track.album.images[0].url ?? null;
 
     return {
       timestamp,
-      song: {
-        title,
-        album,
-        albumImageUrl,
-        artists,
-        spotifyUrl,
-      },
+      song: await createSongObject(recentSong.track),
       isPlaying: false,
     };
   }
 
-  const song = await nowPlaying.json();
-
-  const title = song.item.name;
-  const album = song.item.album.name;
-  const artists = song.item.artists
-    .map((_artist: any) => _artist.name)
-    .join(', ');
-  const spotifyUrl = song.item.external_urls.spotify ?? null;
-  const albumImageUrl = song.item.album.images[0]?.url ?? null;
-
-  return {
-    song: {
-      title,
-      album,
-      albumImageUrl,
-      artists,
-      spotifyUrl,
-    },
-    isPlaying: true,
-  };
+  return { isPlaying: false };
 }
